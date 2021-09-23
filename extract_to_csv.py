6da66260da50e183ced9a204a50d3473ab618bed
Willow Fox Fortino <fortino@udel.edu>
2021-09-23 18:07:14 -0400

    Split the snid_to_csv function into a snid_to_arr and a arr_to_csv function. snid_to_arr will likely be useful later on

--- extract_to_csv.py
+++ extract_to_csv.py
@@ -8,36 +8,63 @@ sys.path.insert(0, codedir)
 import SNIDdataset as snid
 
 
-def snid_to_csv(spec, directory):
+def snid_to_arr(spec):
     """
-    Take necessary information from a SNIDsn object and write it to a csv.
+    Take relevant arrays from a SNIDsn object and return them.
 
     Arguments
     ---------
     spec : SNIDsn object
         This SNIDsn object contains way more information than just the fluxes,
-        wavelengths, and uncertainties that we care about. So I am only
-        extracting the arrays I care about and then writing them to csv files.
-    directory : str
-        The directory to save the csv file to.
+        wavelengths, and uncertainties that we care about. So, extract them
+        separately.
 
     Returns
     -------
-    None
+    SNdata : dict
+        Dictionary containing arrays for the wavelength bin centers, the flux
+        values, the uncertainties on each flux measurement, the phase key
+        (epoch) of the supernova, and the name of the supernova.
     """
     flux = spec.data.astype(float)
     wvl = spec.wavelengths.astype(float)
     phase_key = list(spec.smooth_uncertainty.keys())[0]
     err = spec.smooth_uncertainty[phase_key].astype(float)
 
-    data = {
-        "flux": flux,
+    SNdata = {
         "wvl": wvl,
-        "err": err
+        "flux": flux,
+        "err": err,
+        "phase_key": phase_key,
+        "SN": spec.header["SN"]
     }
-    df = pd.DataFrame(data, columns=["flux", "wvl", "err"])
 
-    name = spec.header["SN"]
+    return SNdata
+
+
+def arr_to_csv(SNdata, directory):
+    """
+    Save wavelength, flux, and uncertainty information in a csv.
+
+    Arguments
+    ---------
+    SNdata : dict
+        Dictionary containing arrays for the wavelength bin centers, the flux
+        values, the uncertainties on each flux measurement, the phase key
+        (epoch) of the supernova, and the name of the supernova.
+
+    Returns
+    -------
+    None
+    """
+    data = {
+        "wvl": SNdata["wvl"],
+        "flux": SNdata["flux"],
+        "err": SNdata["err"]
+    }
+    df = pd.DataFrame(data, columns=["wvl", "flux", "err"])
+
+    name = SNdata["SN"]
     df.to_csv(os.path.join(directory, f"{name}.csv"), index=False)
 
 
