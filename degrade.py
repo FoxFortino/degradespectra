a33b86f11da1f579dd027da353a4afb0471e7b9a
Willow Fox Fortino <fortino@udel.edu>
2022-05-14 17:18:45 -0400

    Testing, multi_spec degrading

--- degrade.py
+++ degrade.py
@@ -8,11 +8,10 @@ import glob
 import utils
 import kernels
 
+import SNIDsn
+
 import numpy as np
 import matplotlib.pyplot as plt
-
-codedir = "/Users/admin/UDel/FASTLab/Summer2021_Research/SESNspectraPCA/code"
-sys.path.insert(0, codedir)
 
 
 def degrade(
@@ -86,7 +85,7 @@ def degrade(
     #     degrading the original spectrum to resolving power R.
 
     """
-    # data_dtype = spec.data.dtype
+    # data_dtype = SNIDobj.data.dtype
 
     # # WWW Here I make an assumption about the units of these quantities. I am
     # # confident that the wavelengths are in units of angstroms, but as far as
@@ -242,13 +241,17 @@ def degrade(
     return new_wvl, new_flux, new_err
 
 
-def degrade_inplace(
-    spec,
+def degrade(
     R,
+    SNIDobj=None,
+    wvl=None,
+    flux=None,
+    uncer=None,
     PSF_width=None,
     spectral_PSF=None,
     PSF_args={},
-    makegif=False
+    makegif=False,
+    savepath="."
         ):
     """
     Re-binning spectra properly with convolution
@@ -257,17 +260,32 @@ def degrade_inplace(
     with Veronique Petit's guidance, this code takes in a spectra with some
     spectral resolution and then rebins it, via convolution, to a new spectral
     resolution, R.
+    
+    I make an assumption about the units of the flux and wavelength arrays as provided by the SESNtemple and the astrodash GitHub repositories. I am confident that the wavelengths are in units of angstroms, but as far as I can tell the units of flux are just some normalized flux units (according to Williamson & Modjaz & Bianco (2019) about SESN PCA) which obviously isn't helpful. However, I have assumed that they have units of F_lambda in cgs units.
 
     Arguments
     ---------
-    spec : SNIDsn object instance
-        Contains information about a particular supernova's spectrum at a
-        particular phase.
     R : float
         Spectral resolution, defined as lambda / (Delta lambda).
 
     Keyword Arguments
     -----------------
+    SNIDobj : SNIDsn object instance, Default: None
+        Contains information about a particular supernova's spectrum at a
+        particular phase. If None, then this program will expect you to supply
+        wave, flux, and uncer kwargs instead.
+    wvl : (N,) array-like, Default: None
+        Array containing center wavelength bins for the spectrograph. If None,
+        then this program will expect you to supply SNIDobj instead.
+    flux : (N,) array-like, Default: None
+        Array containing fluxes for each wavelength bin. If None, then this
+        program will expect you to supply SNIDobj instead.
+    uncer : (N,) array-like or None, Default: None
+        Array containing uncertainties on each flux value. If None and wave and
+        flux kwargs are supplied, then this program will assume that the given
+        spectrum has no uncertainty information. If None and wave and flux
+        aren't supplied then this program will expect SNIDobj to be supplied
+        instead.
     PSF_width : array-like or float, Default: None
         If None, the degradation of the spectra is done by convolving the
         fluxes with a Gaussian with standard deviation proportional to the
@@ -297,176 +315,183 @@ def degrade_inplace(
     makegif : bool, Default: False
         If True, a gif will be made of the convolution which will be saved at
         the image directory (imagedir defined at the top of the script).
+    savepath : str, Default: "."
+        If makegif is True then you can use this kwarg to specify where the resulting gif is saved.
 
     Returns
     -------
-    spec : SNIDsn object instance
+    SNIDobj : SNIDsn object instance
         Contains updated flux, wavelength, and uncertainties corresponding to
         degrading the original spectrum to resolving power R.
-
     """
-    data_dtype = spec.data.dtype
+    if isinstance(SNIDobj, SNIDsn.SNIDsn):
+        wvl = SNIDobj.wavelengths.astype(float)
+        
+        R_current = utils.calc_avg_R(wvl)
+        errmsg = (f"Current R: {R_current}. Chosen R: {R}. "
+                f"You cannot upsample the spectrum. "
+                "Choose a smaller spectral resolution, R.")
+        assert R < R_current, errmsg
 
-    # WWW Here I make an assumption about the units of these quantities. I am
-    # confident that the wavelengths are in units of angstroms, but as far as
-    # I can tell the units of flux are just "normalized flux" which obviously
-    # isn't helpful. However, I have assumed that they have units of F_lambda
-    # in cgs units.
-    wvl = spec.wavelengths.astype(float)  # * u.angstrom
-    flux = spec.data.astype(float)  # * u.erg / u.s / u.cm**2 / u.angstrom
-    # flux -= flux.min()  # This is not necessarily a good thing to do, even though it makes for a prettier plot.
+        # Calculate the left edges of the bins for later on.
+        wvl_LE = utils.adjust_logbins(wvl, current="center", new="leftedge")
+        new_wvl_LE0 = wvl_LE[0]  # Set leftmost bin edge of new bins
+        new_dlam0 = new_wvl_LE0 / R  # Find width of first new bin based on R
+        new_wvl_LE1 = new_wvl_LE0 + new_dlam0  # Find left edge of second bin
+        new_d_loglam = np.log(new_wvl_LE1) - np.log(new_wvl_LE0)  # Find log width
 
-    # Unpack the uncertainties on the spectrum. Assume it has the same units
-    # as the flux.
-    if spec.smooth_uncertainty != {}:
-        uncertainties = True
-        phase_key = list(spec.smooth_uncertainty.keys())[0]
-        err = spec.smooth_uncertainty[phase_key].astype(float)
-    else:
-        uncertainties = False
-
-    R_current = utils.calc_avg_R(wvl)
-    errmsg = (f"Current R: {R_current}. Chosen R: {R}. "
-              f"You cannot upsample the spectrum. "
-              "Choose a smaller spectral resolution, R.")
-    assert R < R_current, errmsg
-
-    # Calculate the left edges of the bins for later on.
-    wvl_LE = utils.adjust_logbins(wvl, current="center", new="leftedge")
-    new_wvl_LE0 = wvl_LE[0]  # Set leftmost bin edge of new bins
-    new_dlam0 = new_wvl_LE0 / R  # Find width of first new bin based on R
-    new_wvl_LE1 = new_wvl_LE0 + new_dlam0  # Find left edge of second bin
-    new_d_loglam = np.log(new_wvl_LE1) - np.log(new_wvl_LE0)  # Find log width
-
-    # Generate the array of new bin left edges based on d_loglam.
-    logwvl_LE0 = np.log(new_wvl_LE0)
-    approx_end = np.log(wvl_LE[-1]) + new_d_loglam
-    new_logwvl_LE = np.arange(logwvl_LE0, approx_end, new_d_loglam)
-    new_wvl_LE = np.exp(new_logwvl_LE)
-    new_wvl = utils.adjust_logbins(new_wvl_LE,
-                                   current="leftedge",
-                                   new="center")
-
-    r = R_current / R
-    if PSF_width is None:
-        # In order to degrade the spectra we convolve it with a Gaussian with a
-        # FWHM proprtional to d_lambda. This necessitates writing our own code
-        # for this convolution because there is no existing code (that we are
-        # aware of) for handling convolutions where the kernel changes at each
-        # step of the integration.
-        # Specifically we assume the FWHM of the appropriate Gaussian kernel is
-        #     FWHM = d_lambda * R_old / R_new
-
-        # Start by generating the array of FWHM for each kernel, and then
-        # converting to an array of standard deviations. I could have written a
-        # Gaussian function that accepts the FWHM as an argument, but I didn't.
-        wvl_LE_plus1 = utils.add_extra_point(wvl_LE)
-        FWHM = np.diff(wvl_LE_plus1) * r
-        stddev = utils.FWHM_to_STD(FWHM)
-
-    elif isinstance(PSF_width, (float, int)):
-        # Let the standard deviation of the Gaussian PSF be constant.
-        stddev = np.ones(wvl_LE.size + 1) * PSF_width
-
-    elif PSF_width.shape == flux.shape:
-        # Let the user define the standard deviation of the Gaussian PSF at
-        # each wavelength
-        stddev = PSF_width
-
-    N = flux.size
-    flux_conv = np.zeros(N)
-    err_conv = np.zeros(N)
-    for i in range(N):
-
-        if spectral_PSF is None:
-            G = kernels.Gaussian(wvl, wvl[i], stddev[i])
+        # Generate the array of new bin left edges based on d_loglam.
+        logwvl_LE0 = np.log(new_wvl_LE0)
+        approx_end = np.log(wvl_LE[-1]) + new_d_loglam
+        new_logwvl_LE = np.arange(logwvl_LE0, approx_end, new_d_loglam)
+        new_wvl_LE = np.exp(new_logwvl_LE)
+        new_wvl = utils.adjust_logbins(new_wvl_LE, current="leftedge", new="center")
+        
+        # The flux data in the SNIDsn objects is in a structured array, so to
+        # recreate that (with my limited understanding of numpy's structured
+        # arrays) I need the resulting number of wavelength buns now.
+        new_Nbins = len(new_wvl)
+        
+        # Generates a list of tuples which contain the column names and dtypes
+        # for the structured array that will hold the new degraded flux values.
+        dtypes = []
+        for phase in SNIDobj.phases:
+            dtypes.append(("Ph" + str(phase), np.float32))
+        new_data = np.zeros(1024, dtype=dtypes)
+        
+        if SNIDobj.smooth_uncertainty == {}:
+            new_uncer_dict = {}
         else:
-            G = spectral_PSF(wvl, wvl[i], stddev[i], **PSF_args)
-
-        # Renormalize the PSF so that the total integral is 1. This is
-        # necessary to handle edge effects.
-        renorm = np.trapz(G, x=wvl)
-        G /= renorm
-
-        # Perform one step of the integration of t
-        flux_conv[i] = np.trapz(flux * G, x=wvl)
+            new_uncer_dict = {phase: None for phase in SNIDobj.phases}
         
-        # For Gaussian convolutions, the errors should be convolved
-        # with the square of the kernel.
-        if uncertainties:
-            err_conv[i] = np.trapz(err * G**2, x=wvl)
-
-        if makegif:
-            if i == 0:
-                gifdir = os.path.join(imagedir, "gif")
-                if os.path.isdir(gifdir):
-                    shutil.rmtree(gifdir)
-                os.mkdir(gifdir)
-                G_max = np.zeros(N)
-
-            G_max[i] = G.max()
-
-            if (i % 5 == 0) or (i in np.arange(N - 5, N)):
-                fig = plt.figure(figsize=(14, 7))
-                plt.plot(wvl, flux, c="k", lw=2,
-                         label="Original Spectrum")
-                plt.plot(wvl, G*r, c="tab:blue", lw=2,
-                         label=f"Kernel * {r:.2f}")
-                plt.plot(wvl, G_max*r, c="gray", ls=":",
-                         label=f"Kernel Max * {r:.2f}", marker="o")
-                plt.plot(wvl, flux_conv, c="tab:orange", lw=2,
-                         label="Convolution")
-                plt.axhline(y=G.max()*r, c="k", ls=":")
-                plt.legend(loc="upper right", fontsize=15)
-
-                plt.title(f"Degrading to R = {R}", fontsize=25)
-                plt.xlabel(r"Wavelength [$\AA$]", fontsize=25)
-                plt.ylabel(r"Normalized Flux [$F_{\lambda}$]", fontsize=25)
-
-                plt.tick_params(axis='both', which='major', labelsize=25)
-                plt.tight_layout()
-
-                plt.ylim((0, None))
-
-                file = os.path.join(gifdir, f"{i:03}.png")
-                plt.savefig(file)
-                fig.clear()
-                plt.close(fig)
-
-            if i == N - 1:
-                filenames = os.path.join(gifdir, "*.png")
-                filenames = sorted(glob.glob(filenames))
-
-                images = []
-                for filename in filenames:
-                    images.append(imageio.imread(filename))
-                file = os.path.join(imagedir, f"degrading_to_{R}.gif")
-                imageio.mimsave(file, images)
-                shutil.rmtree(gifdir)
-
-    # Interpolate the convolved spectra at the new wavelength centers to get
-    # the degraded spectra.
-    new_flux = np.interp(new_wvl, wvl, flux_conv)
-    if uncertainties:
-        new_err = np.interp(new_wvl, wvl, err_conv)
         
-    # new_flux[wvl < wvl[array_starts[0]]] = 0
+        for phase in SNIDobj.phases:
+            # Access the structured array that this data is stored in with the
+            # appropriate phase key, e.g.: "Ph-4.8" for data coresponding to a
+            # phase of -4.8 days.
+            phase_key = "Ph" + str(phase)
+            flux = SNIDobj.data[phase_key].astype(float)
+            
+            # Unpack the uncertainties on the spectrum. Assume it has the same
+            # units as the flux. The SNIDobj.smooth_uncertainty dictionary
+            # might be empty, indicating no uncertainties associated withe
+            # spectra.
+            if SNIDobj.smooth_uncertainty == {}:
+                uncer = None
+            else:
+                uncer = SNIDobj.smooth_uncertainty[phase_key].astype(float)
 
-    # Repack the SNIDsn object with the rebinned wavelength array and the
-    # degraded spectra and errors.
-    if uncertainties:
-        new_err_dict = {}
-        new_err_dict[phase_key] = np.array(new_err)
+            r = R_current / R
+            if PSF_width is None:
+                # In order to degrade the spectra we convolve it with a
+                # Gaussian with a FWHM proprtional to d_lambda. This
+                # necessitates writing our own code for this convolution
+                # because there is no existing code (that we are aware of) for
+                # handling convolutions where the kernel changes at each step
+                # of the integration. Specifically we assume the FWHM of the
+                # appropriate Gaussian kernel is
+                #   FWHM = d_lambda * R_old / R_new
 
-    spec.wavelengths = np.array(new_wvl)
-    spec.data = np.array(new_flux).astype(data_dtype)
-    if uncertainties:
-        spec.smooth_uncertainty = new_err_dict
+                # Start by generating the array of FWHM for each kernel, and
+                # then converting to an array of standard deviations. I could
+                # have written a Gaussian function that accepts the FWHM as an
+                # argument, but I didn't.
+                wvl_LE_plus1 = utils.add_extra_point(wvl_LE)
+                FWHM = np.diff(wvl_LE_plus1) * r
+                stddev = utils.FWHM_to_STD(FWHM)
+
+            elif isinstance(PSF_width, (float, int)):
+                # Let the standard deviation of the Gaussian PSF be constant.
+                stddev = np.ones(wvl_LE.size + 1) * PSF_width
+
+            elif PSF_width.shape == flux.shape:
+                # Let the user define the standard deviation of the Gaussian
+                # PSF at each wavelength
+                stddev = PSF_width
+
+            N = flux.size
+            flux_conv = np.zeros(N)
+            uncer_conv = np.zeros(N)
+            for i in range(N):
+
+                if spectral_PSF is None:
+                    G = kernels.Gaussian(wvl, wvl[i], stddev[i])
+                else:
+                    G = spectral_PSF(wvl, wvl[i], stddev[i], **PSF_args)
+
+                # Renormalize the PSF so that the total integral is 1. This is
+                # necessary to handle edge effects.
+                renorm = np.trapz(G, x=wvl)
+                G /= renorm
+
+                # Perform one step of the integration of t
+                flux_conv[i] = np.trapz(flux * G, x=wvl)
+                
+                # For Gaussian convolutions, the uncertainties should be
+                # convolved with the square of the kernel.
+                if (uncer is not None) and (spectral_PSF is None):
+                    uncer_conv[i] = np.trapz(uncer * G**2, x=wvl)
+
+                if makegif:
+                    if i == 0:
+                        gifdir = os.path.join(savepath, "tmp_gif")
+                        if os.path.isdir(gifdir):
+                            shutil.rmtree(gifdir)
+                        os.mkdir(gifdir)
+                        G_max = np.zeros(N)
+                        
+                    G_max[i] = G.max()
+                        
+                    if (i % 5 == 0) or (i in np.arange(N - 5, N)):
+                        utils.make_frame(wvl, flux, flux_conv,
+                                        G, G_max,
+                                        R, r,
+                                        i, gifdir)
+                    if i == N - 1:
+                        filenames = os.path.join(gifdir, "*.png")
+                        filenames = sorted(glob.glob(filenames))
+
+                        images = []
+                        for filename in filenames:
+                            images.append(imageio.imread(filename))
+                        file = os.path.join(savepath, f"degrading_to_{R}.gif")
+                        imageio.mimsave(file, images)
+                        shutil.rmtree(gifdir)
+
+            # Interpolate the convolved spectra at the new wavelength centers
+            # to get the degraded spectra.
+            new_flux = np.interp(new_wvl, wvl, flux_conv)
+            new_data[phase_key] = new_flux
+            
+            if uncer is not None:
+                new_uncer = np.interp(new_wvl, wvl, uncer_conv)
+                new_uncer_dict[phase_key] = np.array(new_uncer)
+
+        SNIDobj.wavelengths = np.array(new_wvl)
+        SNIDobj.data = new_data
+        if uncer is not None:
+            SNIDobj.smooth_uncertainty = new_uncer_dict
+                
+        SNIDobj.header["Nbins"] = len(SNIDobj.wavelengths)
+    
+    # elif isinstance(wvl, np.ndarray) and isinstance(flux, np.ndarray):
+    #     assert wvl.ndim == 1, f"Wavelengths array must be 1D, is {wvl.ndim}"
+    #     assert flux.ndim == 1, f"Flux array must be 1D, is {wvl.ndim}"
         
-    # WFF TODO
-    # Work out how many bins there should be from 10000 to 2500.
-    # Use bin size or whatever
-    spec.header["Nbins"] = len(spec.wavelengths)
+    #     errmsg = ("Wavelength and flux arrays should have the same shape but "
+    #               "are {flux.shape} and {wvl.shape} respectively.")
+    #     assert wvl.shape == flux.shape, errmsg
+        
+    #     if uncer is not None:
+    #         errmsg = f"Uncertainties array must be 1D, is {wvl.ndim}"
+    #         assert uncer.ndim == 1, errmsg
+            
+    #         errmsg = ("Uncertainties array must be same shape as "
+    #                   f"wavelength and flux arrays but is {uncer.shape} "
+    #                   "while the flux and wavelength arrays are "
+    #                   f"shape {wvl.shape}.")
+    #         assert uncer.shape == wvl.shape, errmsg
+
 
 
 if __name__ == "__main__":
